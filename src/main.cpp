5f877d117ae2afd21185ad9891cac980e13dc858
8-Lambda-8 <j.wasle111@gmail.com>
2023-07-31 15:44:43 +0200

    added MultiDMX lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
+#include <MultiDMX_STM32.h>
+
+PinName pins[DMX_UNIVERSES] = {PA_0, PA_1, PA_2, PA_3};  //, PA_4, PA_5, PB_0, PB_1};
 
 void setup() {
+  Serial.begin(115200);
+
+  MultiDmx.usePins(pins);
 }
 
 void loop() {
