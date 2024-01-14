6ec642aca0314956283817f3709dbfe7474cdf05
8-Lambda-8 <j.wasle111@gmail.com>
2024-01-14 19:09:00 +0100

    changes for ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <MultiDMX.h>
+#include <esp_dmx.h>
 
 #include "Ethernet.h"
 #include "sACN.h"
@@ -12,67 +12,54 @@ EthernetUDP udp;
 Receiver sacnRecievers[] = {Receiver(udp, 1),   //
                             Receiver(udp, 2)};  //
 
-// Receiver(udp, 3),  //
-// Receiver(udp, 4)};
+#define DMX_UNIVERSES 2
 
-PinName pins[DMX_UNIVERSES] = {PB_1, PB_0};  //{32, 33};  //, 25, 26};  //, PA_4, PA_5, PB_0, PB_1};
+uint8_t pins[DMX_UNIVERSES] = {32, 33};
 
 void dmxReceived_(uint8_t u) {
-  //
   Serial.println("Recieved");
-  sacnRecievers[u].dmx((MultiDmx.getBuffer(u)));
+  dmx_write(u + 1, sacnRecievers[u].dmx() - 1, DMX_PACKET_SIZE);
 }
 
 void dmxReceived_0() { dmxReceived_(0); }
 void dmxReceived_1() { dmxReceived_(1); }
-// void dmxReceived_2() { dmxReceived_(2); }
-// void dmxReceived_3() { dmxReceived_(3); };
 
-void (*dmxRecieved[])() = {dmxReceived_0, dmxReceived_1};  //, dmxReceived_2, dmxReceived_3};
+void (*dmxRecieved[])() = {dmxReceived_0, dmxReceived_1};
 
 void newSource() {
   Serial.print("new soure name: ");
   Serial.println(sacnRecievers[0].name());
 }
 
-/* void framerate() {
-  Serial.print("Framerate fps: ");
-  Serial.println(sacnRecievers[0].framerate());
-} */
-
-// void timeOut() { Serial.println("Timeout!"); }
-
 void setup() {
   Serial.begin(115200);
-  Serial.println("begin1");
+  Serial.println("begin");
 
+  Ethernet.init(5);
   if (Ethernet.begin(mac) == 0) {
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    while (Ethernet.hardwareStatus() == EthernetNoHardware) {
       Serial.println("hardware Error");
-      while (true) {
-        delay(1);  // do nothing, no point running without Ethernet hardware
-      }
+      delay(10);
     }
-    Ethernet.begin(mac, ip, dns);
   } else {
+    Ethernet.begin(mac, ip, dns);
     Serial.println(Ethernet.localIP());
   }
-  sacnRecievers[0].callbackSource(newSource);
+
   for (uint8_t i = 0; i < DMX_UNIVERSES; i++) {
     sacnRecievers[i].callbackDMX(dmxRecieved[i]);
-    // sacnRecievers[i].callbackTimeout(timeOut);
-    // sacnRecievers[i].callbackSource(newSource);
+    sacnRecievers[i].callbackSource(newSource);
     sacnRecievers[i].begin();
   }
 
   Serial.println("sACN start");
 
-  MultiDmx.usePins(pins);
+  dmx_config_t config = DMX_CONFIG_DEFAULT;
 
-  MultiDmx.write(0, 6, 255);
-  MultiDmx.write(1, 6, 255);
-  // MultiDmx.write(2, 6, 255);
-  // MultiDmx.write(3, 6, 255);
+  for (uint8_t i = 0; i < DMX_UNIVERSES; i++) {
+    dmx_driver_install(1, &config, DMX_INTR_FLAGS_DEFAULT);
+    dmx_set_pin(i + 1, pins[i], DMX_PIN_NO_CHANGE, DMX_PIN_NO_CHANGE);
+  }
 }
 
 uint32_t loops = 0;
@@ -81,6 +68,7 @@ uint32_t timer = 0;
 void loop() {
   for (uint8_t i = 0; i < DMX_UNIVERSES; i++) {
     sacnRecievers[i].receive();
+    dmx_send(i + 1, DMX_PACKET_SIZE);
   }
   if (millis() - timer > 1000) {
     timer = millis();
@@ -89,19 +77,4 @@ void loop() {
     loops = 0;
   }
   loops++;
-
-  /* MultiDmx.write(0, 1, 0);
-  MultiDmx.write(1, 3, 0);
-  MultiDmx.write(2, 10, 0);
-  MultiDmx.write(3, 12, 0);
-
-  Serial.println(0);
-  delay(2000);
-  MultiDmx.write(0, 1, 255);
-  MultiDmx.write(1, 3, 255);
-  MultiDmx.write(2, 10, 255);
-  MultiDmx.write(3, 12, 255);
-
-  Serial.println(255);
-  delay(2000); */
 }
